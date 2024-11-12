4355eaf7e1798fd072e751e80ef05bfab16d31b1
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-11-12 21:02:44 +0300

    use state

--- Counter/counter/src/App.js
+++ Counter/counter/src/App.js
@@ -8,14 +8,14 @@ function App() {
     setCount(count+1)
   }
   function handleRemove(){
-      setCount(count>=0?count -1:count=0)
+      setCount(count>0?count - 1:count=0)
   }
 
   return (
-      <div>
+      <div className={" flex justify-center mt-2"}>
           <h1>{count}</h1>
-          <button onClick={handleAdd} className={"bg-blue-700"}>add</button>
-          <button onClick={handleRemove} className={"bg-red-700"}>reduce</button>
+          <button onClick={handleAdd} className={"bg-blue-700 mr-1 p-1"}>add</button>
+          <button onClick={handleRemove} className={"bg-red-700 p-1"}>reduce</button>
       </div>
 
 
